b8e258338000e8e52378c7c8afc389cd52db6a6f
Paden Pace <pjpace27@yahoo.com>
2017-05-06 10:40:28 -0500

    basic paths and port export

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,8 +13,8 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/simon/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/html/simon.html"));
+  app.get("/simon", function(req, res) {
+    res.sendFile(path.join(__dirname + "./../public/html/simon.html"));
   });
 
 };
--- server.js
+++ server.js
@@ -29,9 +29,9 @@ require("./routes/api-routes.js")(app);
 //require("./routes/api-route-cust.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-//db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     //console.log(db);
-  //});
+  });
 });
