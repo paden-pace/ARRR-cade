9738b6eb945f9450f2b731954c882154ebf73c2c
rukaroa <rukaroa@gmail.com>
2017-05-06 11:40:49 -0500

    rps object started

--- public/js/rps.js
+++ public/js/rps.js
@@ -1,5 +1,42 @@
-var playerScore = 0;
-var compScore = 0;
-var playerPick;
-var compPick;
+var rps = {
+    //player stats
+    player: {
+        score: 0,
+        pick: 0
+    },
+    //computer stats
+    comp: {
+        score: 0,
+        pick: 0
+    },
+    //take user input from page
+    userInput: function(val) {
+    	
+    },
 
+    //random input 1-3
+    compInput: function() {
+    	//random value from 1-3
+    	var random = Math.floor(Math.random() * 3 + 1);
+    	this.comp.score = random;
+    	console.log(this.comp.score);
+    },
+
+    //compare userInput and compInput
+    compareMoves: function() {
+
+    },
+
+    endRound: function() {
+
+    }
+};
+
+console.log("loaded");
+
+$(".btn").on("click", function() {
+    var value = $(this).attr("data-value");
+    console.log("Value: " + value);
+
+
+});
