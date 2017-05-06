8d00e873ba6ffce9111f73f0e5b92f56ae6301b9
rukaroa <rukaroa@gmail.com>
2017-05-06 13:32:46 -0500

    main gameflow added

--- public/js/rps.js
+++ public/js/rps.js
@@ -9,34 +9,77 @@ var rps = {
         score: 0,
         pick: 0
     },
+    //game flow
+    game: function(val){
+    	this.userInput(val);
+    	this.compInput();
+    	this.compareMoves();
+    	this.endRound();
+    },
     //take user input from page
     userInput: function(val) {
-    	
+    	rps.player.pick = val;
+    	console.log("Player: " + val);
     },
 
     //random input 1-3
     compInput: function() {
-    	//random value from 1-3
-    	var random = Math.floor(Math.random() * 3 + 1);
-    	this.comp.score = random;
-    	console.log(this.comp.score);
+        //random value from 1-3
+        var random = Math.floor(Math.random() * 3 + 1);
+        rps.comp.pick = random;
+        console.log("Comp: " + rps.comp.pick);
     },
 
     //compare userInput and compInput
     compareMoves: function() {
+        var player = rps.player.pick;
+        var comp = rps.comp.pick;
 
+        //1 = rock, 2 = paper, 3 = scissors
+
+        //tie
+        if (player == comp) {
+        	console.log("Draw");
+        }
+        //rock beats scissors
+        else if (player == 1 && comp == 3) {
+            rps.player.score++;
+          	console.log("player win");
+        } else if (comp == 1 && player == 3) {
+        	rps.comp.score++;
+        	console.log("comp win");
+        }
+        //scissors beats paper, paper beats rock
+        else if (player > comp) {
+            rps.player.score++;
+            console.log("player win");
+        } else {
+            rps.comp.score++
+            console.log("comp win");
+        }
     },
-
+    //decide if game ends
     endRound: function() {
+        var player = rps.player.score;
+        var comp = rps.comp.score;
 
+        if (player == 3) {
+            console.log("You win");
+            //return win to database
+            //return to main page
+        } else if (comp == 3) {
+            console.log("You lose");
+            //display snarky message
+            //return to main page
+        } else {
+            //next round
+        }
     }
 };
 
 console.log("loaded");
 
-$(".btn").on("click", function() {
+$(".player").on("click", function(){
     var value = $(this).attr("data-value");
-    console.log("Value: " + value);
-
-
+    rps.game(value);
 });
