a8f8bc6b290f774e3167ff988076fb43d287f7b2
Paden Pace <pjpace27@yahoo.com>
2017-05-13 10:02:38 -0500

    heroku workability

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
   // index route loads view.html
 
   app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname + "./../public/view.html"))
+    res.sendFile(path.join(__dirname + "./../view.html"))
   })
 
   app.get("/pog", function(req, res){
